3d5783b17f254366c45228508c926c1b93c74785
Timmy <Timmy224@gmail.com>
2019-08-27 11:36:46 -0400

    update PER-all seasons

--- PER test/PER-allSeasons.py
+++ PER test/PER-allSeasons.py
@@ -11,47 +11,47 @@ def PERCalculation():
     PER_list = []
 
     for i in range(1998, 2019):
-        df_players = df_players[df_players['Season'] == '{}-{}'.format(i, i+1)]
-        df_teams = df_teams[df_teams['Season'] == '{}-{}'.format(i, i+1)]
+        df_players_inst = df_players[df_players['Season'] == '{}-{}'.format(i, i+1)]
+        df_teams_inst = df_teams[df_teams['Season'] == '{}-{}'.format(i, i+1)]
 
         # league totals and calculations
-        lg_AST = df_players['AST'].mean()
-        lg_FG = df_players['FG'].mean()
-        lg_FT = df_players['FT'].mean()
+        lg_AST = df_players_inst['AST'].mean()
+        lg_FG = df_players_inst['FG'].mean()
+        lg_FT = df_players_inst['FT'].mean()
 
         factor = (2 / 3) - (0.5 * (lg_AST / lg_FG)) / (2 * (lg_FG / lg_FT))
         
-        lg_PTS = df_players['PTS'].mean()
-        lg_FGA = df_players['FGA'].mean()
-        lg_ORB = df_players['ORB'].mean()
-        lg_TOV = df_players['TOV'].mean()
-        lg_FTA = df_players['FTA'].mean()
+        lg_PTS = df_players_inst['PTS'].mean()
+        lg_FGA = df_players_inst['FGA'].mean()
+        lg_ORB = df_players_inst['ORB'].mean()
+        lg_TOV = df_players_inst['TOV'].mean()
+        lg_FTA = df_players_inst['FTA'].mean()
 
         VOP = lg_PTS / (lg_FGA - lg_ORB + lg_TOV + 0.44 * lg_FTA)
 
-        lg_TRB = df_players['TRB'].mean()
+        lg_TRB = df_players_inst['TRB'].mean()
         
         DRB_perc  = (lg_TRB - lg_ORB) / lg_TRB
 
         # uPER variables and calculation
-        names = df_players['Player']
-        player_teams = df_players['Tm']
-        MP = df_players['MP']
-        player_3P = df_players['3P']
-        AST = df_players['AST']
-        FG = df_players['FG']
-        FT = df_players['FT']
-        TOV = df_players['TOV']
-        FGA = df_players['FGA']
-        FTA = df_players['FTA']
-        TRB = df_players['TRB']
-        ORB = df_players['ORB']
-        STL = df_players['STL']
-        BLK = df_players['BLK']
-        PF = df_players['PF']
+        names = df_players_inst['Player']
+        player_teams = df_players_inst['Tm']
+        MP = df_players_inst['MP']
+        player_3P = df_players_inst['3P']
+        AST = df_players_inst['AST']
+        FG = df_players_inst['FG']
+        FT = df_players_inst['FT']
+        TOV = df_players_inst['TOV']
+        FGA = df_players_inst['FGA']
+        FTA = df_players_inst['FTA']
+        TRB = df_players_inst['TRB']
+        ORB = df_players_inst['ORB']
+        STL = df_players_inst['STL']
+        BLK = df_players_inst['BLK']
+        PF = df_players_inst['PF']
 
         # team variables
-        unique_teams = list(set(df_players['Tm']))
+        unique_teams = list(set(df_players_inst['Tm']))
 
         """ need to figure out team name changes """
         # 2-way dict used to convert full team name to abbreviation and vice-versa
@@ -59,8 +59,9 @@ def PERCalculation():
             'Atlanta Hawks': 'ATL',
             'Boston Celtics': 'BOS',
             'Brooklyn Nets': 'BRK',
-            'Charlotte Hornets': 'CHI',
-            'Chicago Bulls': 'CHO',
+            'Charlotte Bobcats': 'CHA',
+            'Charlotte Hornets': ['CHH', 'CHO'],
+            'Chicago Bulls': 'CHI',
             'Cleveland Cavaliers': 'CLE',
             'Dallas Mavericks': 'DAL',
             'Denver Nuggets': 'DEN',
@@ -74,6 +75,9 @@ def PERCalculation():
             'Miami Heat': 'MIA',
             'Milwaukee Bucks': 'MIL',
             'Minnesota Timberwolves': 'MIN',
+            'New Jersey Nets': 'NJN',
+            'New Orleans Hornets': 'NOH',
+            'New Orleans/Oklahoma City Hornets': 'NOK',
             'New Orleans Pelicans': 'NOP',
             'New York Knicks': 'NYK',
             'Oklahoma City Thunder': 'OKC',
@@ -83,61 +87,49 @@ def PERCalculation():
             'Portland Trail Blazers': 'POR',
             'Sacramento Kings': 'SAC',
             'San Antonio Spurs': 'SAS',
+            'Seattle SuperSonics': 'SEA',
             'Toronto Raptors': 'TOR',
             'Utah Jazz': 'UTA',
+            'Vancouver Grizzlies': 'VAN',
             'Washington Wizards': 'WAS',
-            'ATL': 'Atlanta Hawks',
-            'BOS': 'Boston Celtics',
-            'BRK': 'Brooklyn Nets',
-            'CHI': 'Charlotte Hornets',
-            'CHO': 'Chicago Bulls',
-            'CLE': 'Cleveland Cavaliers',
-            'DAL': 'Dallas Mavericks',
-            'DEN': 'Denver Nuggets',
-            'DET': 'Detroit Pistons',
-            'GSW': 'Golden State Warriors',
-            'HOU': 'Houston Rockets',
-            'IND': 'Indiana Pacers',
-            'LAC': 'Los Angeles Clippers',
-            'LAL': 'Los Angeles Lakers',
-            'MEM': 'Memphis Grizzlies',
-            'MIA': 'Miami Heat',
-            'MIL': 'Milwaukee Bucks',
-            'MIN': 'Minnesota Timberwolves',
-            'NOP': 'New Orleans Pelicans',
-            'NYK': 'New York Knicks',
-            'OKC': 'Oklahoma City Thunder',
-            'ORL': 'Orlando Magic',
-            'PHI': 'Philadelphia 76ers',
-            'PHO': 'Phoenix Suns',
-            'POR': 'Portland Trail Blazers',
-            'SAC': 'Sacramento Kings',
-            'SAS': 'San Antonio Spurs',
-            'TOR': 'Toronto Raptors',
-            'UTA': 'Utah Jazz',
-            'WAS': 'Washington Wizards',
         }
 
-        # adding tm col to df_teams 
+        # Charlotte Hornets name splits 
+        CHH_season = []
+        for i in range(1998, 2002):
+            CHH_season.append('{}-{}'.format(i, i+1))
+
+        CHO_season = []
+        for i in range(2014, 2019):
+            CHO_season.append('{}-{}'.format(i, i+1))
+
+        # adding tm col to df_teams_inst 
         tm_list = []
-        for each in df_teams['Team']:
-            tm_list.append(team_tm[each])
-        df_teams.insert(4, 'Tm', tm_list)
+        for each_team, season in zip(df_teams_inst['Team'], df_teams_inst['Season']):
+            if each_team == 'Charlotte Hornets':
+                if season in CHH_season:
+                    tm_list.append(team_tm['Charlotte Hornets'][0])
+                else: 
+                    tm_list.append(team_tm['Charlotte Hornets'][1])
+            else:
+                tm_list.append(team_tm[each_team])
+        df_teams_inst.insert(4, 'Tm', tm_list)
+
 
         team_AST, team_FG, team_Pace = {}, {}, {}
         for each_team in unique_teams:
-            team_AST[each_team] = df_players[df_players['Tm'] == each_team]['AST'].mean()
-            team_FG[each_team] = df_players[df_players['Tm'] == each_team]['FG'].mean()
-            team_Pace[each_team] = df_teams[df_teams['Tm'] == each_team]['Pace']
+            team_AST[each_team] = df_players_inst[df_players_inst['Tm'] == each_team]['AST'].mean()
+            team_FG[each_team] = df_players_inst[df_players_inst['Tm'] == each_team]['FG'].mean()
+            team_Pace[each_team] = df_teams_inst[df_teams_inst['Tm'] == each_team]['Pace']
             #print(each_team, team_AST[each_team], team_FG[each_team], team_Pace[each_team])
         
         # league variables
-        lg_FT = df_players['FT'].mean()
-        lg_FTA = df_players['FTA'].mean()
-        lg_PF = df_players['PF'].mean()
+        lg_FT = df_players_inst['FT'].mean()
+        lg_FTA = df_players_inst['FTA'].mean()
+        lg_PF = df_players_inst['PF'].mean()
         
         #Figure out way to use player team abb to link to team full team 
-        lg_Pace = df_teams['Pace'].mean()
+        lg_Pace = df_teams_inst['Pace'].mean()
         
         aPER_list = []
         # maybe DRB% is not league DRB? - see if calculations work first
@@ -146,25 +138,30 @@ def PERCalculation():
         each_PF in zip(names, player_teams, MP, player_3P, AST, FG, FT, TOV, FGA, FTA, TRB, ORB, \
         STL, BLK, PF): 
 
-            uPER = (1 / each_MP) * \
-                (each_player_3P \
-                + (2/3) * each_AST + (2 - factor \
-                * (team_AST[each_player_team] / team_FG[each_player_team])) \
-                * each_FG + (each_FT * 0.5 * (1 + (1 - (team_AST[each_player_team] \
-                / team_FG[each_player_team])) + (2/3) 
-                * (team_AST[each_player_team] / team_FG[each_player_team]))) \
-                - VOP * each_TOV \
-                - VOP * DRB_perc * (each_FGA - each_FG) \
-                - VOP * 0.44 * (0.44 + (0.56 * DRB_perc)) * (each_FTA - each_FT) \
-                + VOP * (1 - DRB_perc) * (each_TRB - each_ORB) \
-                + VOP * DRB_perc * each_ORB \
-                + VOP * each_STL \
-                + VOP * DRB_perc * each_BLK \
-                - each_PF * ((lg_FT / lg_PF) - 0.44 * (lg_FTA / lg_PF) * VOP))
+            try:
+                uPER = (1 / each_MP) * \
+                    (each_player_3P \
+                    + (2/3) * each_AST + (2 - factor \
+                    * (team_AST[each_player_team] / team_FG[each_player_team])) \
+                    * each_FG + (each_FT * 0.5 * (1 + (1 - (team_AST[each_player_team] \
+                    / team_FG[each_player_team])) + (2/3) 
+                    * (team_AST[each_player_team] / team_FG[each_player_team]))) \
+                    - VOP * each_TOV \
+                    - VOP * DRB_perc * (each_FGA - each_FG) \
+                    - VOP * 0.44 * (0.44 + (0.56 * DRB_perc)) * (each_FTA - each_FT) \
+                    + VOP * (1 - DRB_perc) * (each_TRB - each_ORB) \
+                    + VOP * DRB_perc * each_ORB \
+                    + VOP * each_STL \
+                    + VOP * DRB_perc * each_BLK \
+                    - each_PF * ((lg_FT / lg_PF) - 0.44 * (lg_FTA / lg_PF) * VOP))
+            except ZeroDivisionError:
+                uPER = 0
 
             pace_adj = lg_Pace / team_Pace[each_player_team].values
             aPER = pace_adj * uPER
             aPER_list.append(aPER.item())
+            
+    
 
         # lg_aPER
         lg_aPER = statistics.mean(aPER_list)
@@ -176,8 +173,12 @@ def PERCalculation():
         #print('factor', factor)
         #print('VOP', VOP)
         #print('DRB%', DRB_perc)
-
+    
+        #df_players_inst.insert(6, 'PER_calc', PER_list)
+        #print(df_players_inst)
+    print(PER_list)  
     df_players.insert(6, 'PER_calc', PER_list)
     print(df_players)
-     
+    df_players.to_csv('test.csv')
+    
 PERCalculation()
